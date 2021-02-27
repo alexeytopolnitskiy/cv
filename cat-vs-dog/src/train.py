1441069ff92dad674a7e9b5268be11987d6b3404
Alexey Topolnitskiy <alextopol2903@gmail.com>
2021-02-27 21:13:54 +0300

    add comments

--- cat-vs-dog/src/train.py
+++ cat-vs-dog/src/train.py
@@ -13,7 +13,7 @@ import config
 import model_dispatcher
 
 def run():
-    # create transformations for images
+    # create transformations for train and test images
     transformation = {
         "train": 
         transforms.Compose([
@@ -62,11 +62,8 @@ def run():
         num_workers=2
     )
 
-    # initialize model
+    # initialize the model
     model = model_dispatcher.CatDogNN(config.MODEL_IN_FEATURES, config.MODEL_OUT_FEATURES)
-
-    # load parameters of the model
-    model.load_state_dict(torch.load(config.BEST_MODEL))
 
     # send model to cuda (if available) for faster training
     model = model.to(config.TRAIN_DEVICE)
@@ -84,50 +81,55 @@ def run():
     # run the train loop
     for epoch in range(epochs):
         train_loss = 0
+        # set the model to train mode
         model.train()
+        # iterate over train data
         for data in train_dataloader:
             inputs = data[0]
             targets = data[1]
-            
+            # send data to device
             inputs = inputs.to(config.TRAIN_DEVICE)
             targets = targets.to(config.TRAIN_DEVICE)
-
+            # make predictions and update model's parameters
             optimizer.zero_grad()
             outputs = model(inputs)
             loss = criterion(outputs, targets)
             loss.backward()
             optimizer.step()
             train_loss += loss.item()*inputs.size(0)
-        
+        # evaluate model on test data
         test_loss = 0
+        # set the model to evaluation mode
         model.eval()
         correct = 0
         total = 0
+        # turn off gradient calculations
         with torch.no_grad():
+            # iterate over test data
             for data in test_dataloader:
                 inputs = data[0]
                 targets = data[1]
                 inputs = inputs.to(config.TRAIN_DEVICE)
                 targets = targets.to(config.TRAIN_DEVICE)
                 outputs = model(inputs)
-
+                # calculate loss and accuracy of predictions
                 tloss = criterion(outputs, targets)
                 test_loss += tloss.item()*inputs.size(0)
                 outputs = torch.max(outputs, dim=1)[1]
                 correct += (targets == outputs).sum()
                 total += len(targets)
-
+        # calculate total losses and total accuracy of the epoch
         train_loss = train_loss / len(train_dataloader.sampler)
         train_losses.append(train_loss)
         test_loss = test_loss / len(test_dataloader.sampler)
         test_losses.append(test_loss)
         acc = correct / float(total)
-
+        # if accuracy improved save this model as the best 
         if acc > best_acc:
             best_model = copy.deepcopy(model)
             best_acc = acc
             print("Model improved!!!")
-
+        # print results of the epoch
         print(f"Epoch: {epoch}. Accuracy: {round(acc.detach().cpu().numpy().tolist(), 4)}. Best accuracy: {round(best_acc.detach().cpu().numpy().tolist(), 4)} Train Loss: {round(train_loss, 4)}. Test Loss: {round(test_loss, 4)}")
 
     # save the best model parameters after training
